cba89b4ff403ebc1b8be4e1b47d6b3296a2839e5
Jhan Arevalo <carlos2525x@gmail.com>
2024-10-31 00:24:01 -0500

    agregar tittle

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <title>JhanAreDev</title>
       <Head />
       <body className="font-primary">
         <Main />
